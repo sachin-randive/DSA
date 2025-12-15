df736581ab0bbde14b8d27e352b8a4cc5e70ee04
Sachin Randive <r.sachin81@gmail.com>
2025-12-15 10:15:24 +0530

    SearchArray

--- Array/SearchArray.playground/Contents.swift
+++ Array/SearchArray.playground/Contents.swift
@@ -1,3 +1,34 @@
 import UIKit
+/*
+ 
+ Given an array of numbers, determines if the array contains a targer value
+ 
+ Example 1:
+ Input = [1, 4, 2, 18, 16, 8], target = 6
+ Output = true
+ 
+ Example 2:
+ 
+ Input = [1, 4, 2, 18, 16, 8], target = 23
+ Output = false
+ 
+ Constraint:
+ - Don't use .contains(_ value: Int)
+ 
+ Follow up:
+ - Given that the array is sorted in ascending order, write an algorithm that in O(nlogn) time.
+ 
+ */
 
-var greeting = "Hello, playground"
+
+func arrayContains(_ arr: [Int], _ target: Int) -> Bool {
+    
+    for num in arr {
+        if num == target {
+            return true
+        }
+    }
+    return false
+}
+
+arrayContains([1, 4, 2, 18, 16, 8], 2)
