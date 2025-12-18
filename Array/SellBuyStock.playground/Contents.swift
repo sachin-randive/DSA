6f3d4273f6ba002a518b4ee92f4f1ab05f557401
Sachin Randive <r.sachin81@gmail.com>
2025-12-18 15:05:04 +0530

    sell and buy stock

---
+++ Array/SellBuyStock.playground/Contents.swift
@@ -0,0 +1,44 @@
+import UIKit
+
+/*
+Leetcode 121 – Best Time to Buy and Sell Stock
+Difficulty: Easy
+Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+Prompt:
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6 - 1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0.
+*/
+
+func findMaxProfit(_ prices: [Int]) -> Int {
+    var maxProfit = 0
+    
+    for i in 0..<prices.count - 1 {
+        print(prices[i])
+        for j in i+1..<prices.count {
+            let profit = prices[j] - prices[i]
+            maxProfit = max(maxProfit, profit)
+        }
+    }
+    
+    return maxProfit
+}
+
+findMaxProfit( [7,1,5,3,6,4] )
+print(findMaxProfit( [7,1,5,3,6,4] ))
